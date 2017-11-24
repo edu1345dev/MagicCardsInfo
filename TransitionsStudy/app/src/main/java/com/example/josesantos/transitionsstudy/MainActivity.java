1bad816e7fbc63654b0c93dc0a0a52c5fdb3bcad
Jose Santos <jose.santos@pixida.com.br>
2017-11-24 17:41:21 -0200

    search for store prizes on html

--- TransitionsStudy/app/src/main/java/com/example/josesantos/transitionsstudy/MainActivity.java
+++ TransitionsStudy/app/src/main/java/com/example/josesantos/transitionsstudy/MainActivity.java
@@ -6,7 +6,9 @@ import android.support.constraint.ConstraintLayout;
 import android.support.constraint.ConstraintSet;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.Html;
+import android.text.TextWatcher;
 import android.transition.ChangeBounds;
 import android.transition.Fade;
 import android.transition.Slide;
@@ -16,7 +18,13 @@ import android.transition.TransitionSet;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.DataNode;
@@ -31,91 +39,143 @@ import java.io.IOException;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
-    ConstraintSet constraintSet1 = new ConstraintSet();
-    ConstraintSet constraintSet2 = new ConstraintSet();
 
-    ConstraintLayout container;
-    private boolean isExpanded = true;
-    private TextView tv;
+    private EditText etNomeCarta;
+    private ImageView ivImagem;
+    private TextView tvRaridade;
+    private TextView tvMenorPreco;
+    private TextView tvNomeEdicao;
+    private ImageButton btBusca;
+    private String queryValue;
+    private TextView tvMedioPreco;
+    private TextView tvMaiorPreco;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        container = findViewById(R.id.container);
-        tv = findViewById(R.id.text);
+        tvRaridade = findViewById(R.id.raridade);
+        tvNomeEdicao = findViewById(R.id.nome_edicao);
+        tvMenorPreco = findViewById(R.id.menor_preco);
+        tvMedioPreco = findViewById(R.id.medio_preco);
+        tvMaiorPreco = findViewById(R.id.maior_preco);
+        etNomeCarta = findViewById(R.id.busca);
+        ivImagem = findViewById(R.id.image);
+        btBusca = findViewById(R.id.bt_busca);
 
-//        tv.setText(Html.fromHtml(Consntants.cardsList));
+        configureQuery();
+
+        startQuery("Atog");
+    }
+
+    private void configureQuery() {
+        btBusca.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startQuery(etNomeCarta.getText().toString());
+            }
+        });
+    }
+
+    private void startQuery(String query) {
+        queryValue = query.replace(" ","%20");
 
         HandlerThread thread = new HandlerThread("thread");
         thread.start();
 
         Handler handler = new Handler(thread.getLooper());
-        handler.postDelayed(new Runnable() {
+        handler.post(new Runnable() {
             @Override
             public void run() {
                 getLigamagicPage();
             }
-        },100);
-
-//        constraintSet1.clone(container);
-//        constraintSet2.clone(getBaseContext(), R.layout.activity_main_expanded);
-//
-//        container.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                applyTransition();
-//            }
-//        });
+        });
     }
 
     private void getLigamagicPage() {
         try {
-            final Document doc1 = Jsoup.connect("http://www.ligamagic.com.br/?view=cards/card&card=Hazoret%20the%20Fervent").get();
+            final Document doc1 = Jsoup.connect("http://www.ligamagic.com.br/?view=cards/card&card="+queryValue ).get();
 
-            Log.d(TAG, "getLigamagicPage: "+doc1.getAllElements());
+            doc1.getAllElements().filter(new NodeFilter() {
+                @Override
+                public FilterResult head(Node node, int depth) {
+
+                    Log.d(TAG, "head Node "+node);
+
+                    String attr = node.attr("type").toString();
+
+                    Log.d(TAG, "head from Node "+attr);
+
+                    TextView textView = new TextView(getBaseContext());
+                    textView.setText(attr);
+
+                    String fromTv = (String) textView.getText();
+
+                    Log.d(TAG, "head texView: "+fromTv);
+
+                    String replaced = fromTv.replace("\t","").replace("\n","").replace("<","").replace(">","");
+
+                    Log.d(TAG, "head replaced: "+replaced);
+                    return null;
+                }
+
+                @Override
+                public FilterResult tail(Node node, int depth) {
+//                    Log.d(TAG, "tail: "+node);
+                    return null;
+                }
+            });
 
             Elements scriptElements = doc1.getElementsByTag("script");
 
             for (Element element: scriptElements) {
                 for (DataNode node : element.dataNodes()){
                     if (node.getWholeData().contains("VETiRaridade")){
-
+                        MagicInfoParser parser = new MagicInfoParser();
+                        parser.parse(node.getWholeData());
+                        handleOnMainThread(parser);
                     }
-                    Log.d(TAG, "data nodes: "+ node.getWholeData());
                 }
             }
 
-            Log.d(TAG, "------------------------------------------------------");
+            Elements divElements = doc1.getElementsByTag("div");
+
+            for (Element element: divElements) {
+                for (DataNode node : element.dataNodes()){
+                    Log.d(TAG, "div "+node.getWholeData());
+//                    if (node.getWholeData().contains("VETiRaridade")){
+//                        MagicInfoParser parser = new MagicInfoParser();
+//                        parser.parse(node.getWholeData());
+//                        handleOnMainThread(parser);
+//                    }
+                }
+            }
 
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private void applyTransition() {
-        if (isExpanded){
-            TransitionSet transition = new TransitionSet();
-            transition.addTransition(new ChangeBounds());
-            transition.addTransition(new Slide());
+    private void handleOnMainThread(final MagicInfoParser parser) {
+        Handler handler = new Handler(getMainLooper());
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                showContent(parser);
+            }
+        });
+    }
 
+    private void showContent(MagicInfoParser parser) {
+        Glide.with(this)
+                .load(parser.getListImagem().get(0))
+                .into(ivImagem);
 
-            TransitionManager.beginDelayedTransition(container, transition);
-            constraintSet2.applyTo(container);
-
-            isExpanded = false;
-        }else {
-            TransitionSet transition = new TransitionSet();
-            transition.addTransition(new ChangeBounds());
-            transition.addTransition(new Slide());
-
-
-            TransitionManager.beginDelayedTransition(container, transition);
-
-            constraintSet1.applyTo(container);
-
-            isExpanded = true;
-        }
+        tvRaridade.setText("Raridade: "+parser.getListRaridade().get(0));
+        tvNomeEdicao.setText("Edição: "+parser.getListNomeOficial().get(0));
+        tvMenorPreco.setText(parser.getListMenorPreco().get(0));
+        tvMedioPreco.setText(parser.getListMedioPreco().get(0));
+        tvMaiorPreco.setText(parser.getListMaiorPreco().get(0));
     }
 }
